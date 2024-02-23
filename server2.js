7dfed79c014e16a85585a69146a2a7c6f8ef7f7c
muner12 <bakhtmuner06@gmail.com>
2024-02-23 14:23:33 +0500

    new changes by munir

--- server2.js
+++ server2.js
@@ -1,14 +1,52 @@
 const http=require('http');
+const fs=require('fs');
 
 
-let server=http.createServer((...rest)=>{
-    let data={...rest}
-    let req=data[0];
-    let res=data[1];
-    res.writeHead(200);
-  res.end(JSON.stringify({message:"Success"}));
-});
+let print=(data)=>{
+     console.log(data);
+}
 
-server.listen(8080,()=>{
-    console.log('listening on port 8080');
-});
+const url=new URL("https://user:pass@sub.example.com:8080/p/a/t/h?query=string#hash");
+
+
+print(url.host);
+print(url.pathname);
+print(url.username);
+print(url.password);
+print(url.hash);
+print(url.port);
+print(url.search);
+print(url.searchParams);
+print(url.origin)
+print(url.href)
+
+
+// let server=http.createServer((...rest)=>{
+//     let data={...rest}
+//     let req=data[0];
+//     let res=data[1];
+//     //read file asynchronously 
+//     fs.readFile("db/users.json",'utf-8',(error,data)=>{
+//         if(error){
+//             console.log("Error reading db.json");
+//             res.end("error while reading db.json");
+//         }else{
+//             console.log("file read asynchronously");
+//             res.end(data.toString());
+
+//         }
+
+
+//     });
+//     console.log("done");
+//    // fs.readFileSync("db/users.json",'utf-8')
+
+
+
+//     res.writeHead(200);
+
+
+// });
+
+
+// server.listen(8080);
